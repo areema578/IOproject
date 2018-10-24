594bd6a02d95ea36bea16dac7e20a9b3f788371a
Howard M Chen <31971182+hchen88@users.noreply.github.com>
2018-10-24 00:31:14 -0700

    Add files via upload

--- EarthText.java
+++ EarthText.java
@@ -1,5 +1,13 @@
 package projectIO;
-
+/**	
+ * Scott Arima and Howard Chen
+ * 24 October 2018
+ * Purpose of the program- To demonstrate Handling Exceptions using File I/O and 
+ * the adapter Design pattern with a Star Trek's Universal Translator for today's 
+ * cell phones.
+ * Inputs: N/A
+ * Output: Exception Messages, CellPhone actions and translated texts.
+ */
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
--- InvalidLanguageException.java
+++ InvalidLanguageException.java
@@ -1,5 +1,13 @@
 package projectIO;
-
+/**	
+ * Scott Arima and Howard Chen
+ * 24 October 2018
+ * Purpose of the program- To demonstrate Handling Exceptions using File I/O and 
+ * the adapter Design pattern with a Star Trek's Universal Translator for today's 
+ * cell phones.
+ * Inputs: N/A
+ * Output: Exception Messages, CellPhone actions and translated texts.
+ */
 public class InvalidLanguageException extends Exception {
 	
 	private String languageType = "N/A";
